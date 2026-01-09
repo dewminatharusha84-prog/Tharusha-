d38a37945bbef85fa3fc54ef18c339287def69bf
dewminatharusha84-prog <dewminatharusha84@gmail.com>
2026-01-09 21:43:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'XV7WZ31H' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'HAJZ1JHE' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
